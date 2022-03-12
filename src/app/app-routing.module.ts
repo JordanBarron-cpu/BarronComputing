b94740151ddec1e3f3af3b66afa80ccb69c47d12
Jordan Barron <jordanbarron79@yahoo.com>
2022-03-12 16:24:42 -0500

    Router links set up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppDevComponent } from './app-dev/app-dev.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomepageComponent},
+  {path: 'applicationDevelopment', component: AppDevComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [HomepageComponent, AppDevComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolbarNavComponent } from './toolbar-nav/toolbar-nav.component';
-import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToolbarNavComponent,
-    HomepageComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
